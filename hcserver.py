9ad9d41bd2de8cb0c2cf8707b03cf101227fbe45
deju <kdjteda@gmail.com>
2018-12-26 17:20:55 +0800

    server updated

--- hcserver.py
+++ hcserver.py
@@ -1,31 +1,27 @@
 from flask import Flask
 import json
+from flask_restful import Api
+from flask_restful import Resource
+from flask import request
+from commands import commands
 
-app = Flask("__name__")
+app = Flask(__name__)
+api = Api(app)
 
+class commands_(Resource):
 
-@app.route("/commands/")
-def commands():
-    command_content = "print 'hello world!'"
+    def get(self):
+        command = commands.get()
+        out = {"content":command}
+        return out,200
 
-    out = {"content":command_content}
-    return json.dumps(out)
+    def post(self):
+        data = json.loads(request.data)
+        cmd = data["content"]
+        commands.post(cmd)
 
-
-@app.route("/register/")
-def register():
-    return "register"
-
-
-@app.route("/task/")
-def task():
-    return ""
-
-
-@app.route("/getretrun/")
-    return ""
-
-
+api.add_resource(commands_,"/commands/")
+   
 
 if __name__ == "__main__":
     app.run(port=5001,debug=True)
